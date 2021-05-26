4c2e3281eecd4bb7edcabef31ed75bbe12252391
claireluchen <81056267+claireluchen@users.noreply.github.com>
2021-05-26 11:30:08 -0400

    Update StringHashing.java

--- StringHashing.java
+++ StringHashing.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 /*
 Use string hashing (rolling hash precomputation) to determine if str contains sub
 
@@ -21,30 +22,34 @@ hash(l, r) = hash(1, r) – hash(1, l – 1) * B ^ (r – l + 1)
 Ex: OR = 23 * (29 ^ 2) + 15 * 29 + 18 - 23 * (29 ^ 2)
 */
 public class Main {
-
-	public static void main(String[] args) throws IOException{
+  public static void main(String[] args) throws IOException{
     final int MOD = 1000000009;
     Scanner in = new Scanner(System.in);
-    String str = in.next(), sub = in.next();
+    String str = in.next(), sub = in.next(); //get the index of sub in str
     //calculate the hash of sub as we get the prefix array of str
-    int[] prefixArray = new int[str.length() + 1];
-    int subHash = 0;
+    //use 2 base values (29 and 31) to decrease the chance of accidental collision
+    int[] prefixArray29 = new int[str.length() + 1];
+    int[] prefixArray31 = new int[str.length() + 1];
+    int subHash29 = 0;
+    int subHash31 = 0;
     int j = 0;
-    for (int i = 1; i < prefixArray.length; i++){
-      prefixArray[i] = (29 * prefixArray[i - 1] + ((int) str.charAt(i - 1) - 65)) % MOD;
+    for (int i = 1; i < prefixArray29.length; i++){
+      prefixArray29[i] = (29 * prefixArray29[i - 1] + ((int) str.charAt(i - 1) - 65)) % MOD;
+      prefixArray31[i] = (31 * prefixArray31[i - 1] + ((int) str.charAt(i - 1) - 65)) % MOD;
       if (j < sub.length()){
-        subHash *= 29;
-        subHash += (int) sub.charAt(j) - 65;
+        subHash29 *= 29; subHash29 += (int) sub.charAt(j) - 65;
+        subHash31 *= 31; subHash31 += (int) sub.charAt(j) - 65;
         j++;
       }
     }
     
     //do the checking
-    for (int i = 1; i + sub.length() - 1 < prefixArray.length; i++){
-      int temp = prefixArray[i + sub.length() - 1] - prefixArray[i - 1] * (int) Math.pow(29, sub.length());
-      if (temp == subHash){
+    for (int i = 1; i + sub.length() - 1 < prefixArray29.length; i++){
+      int temp29 = prefixArray29[i + sub.length() - 1] - prefixArray29[i - 1] * (int) Math.pow(29, sub.length());
+      int temp31 = prefixArray31[i + sub.length() - 1] - prefixArray31[i - 1] * (int) Math.pow(31, sub.length());
+      if (temp29 == subHash29 && temp31 == subHash31){
         System.out.println("sub appears in str at index " + i);
       }
     }
-	}
+  }
 }
