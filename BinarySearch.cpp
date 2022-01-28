dadedf8951da02c5491df7970d383f03be1171fd
Claire Chen <81056267+claireluchen@users.noreply.github.com>
2022-01-28 08:22:32 -0500

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,30 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int maxn = 1e5 + 5;
+typedef long long ll;
+const int maxn = 5005;
+const int mod = 1e9 + 7;
 
-int n, target;
-int arr[maxn];
+int n;
 
-int binarySearch(int low, int high){
-  if (low == high) return low;
-  if (low + 1 == high){
-    if (arr[low] < target) return high;
-    else return low;
+int main() {
+  ios_base::sync_with_stdio(false); 
+  cin.tie(0); cout.tie(0);
+  
+  int l = 0, r = n, mid;
+  while(l < r){
+    mid = (l + r) / 2;
+    if (ok(mid)){ //if mid works, search in lower half, including mid
+      r = mid;
+    }else{ //if mid does not work, search in upper half, excluding mid
+      l = mid + 1;
+    }
   }
-  int mid = (low + high) / 2;
-  if (arr[mid] < target) return binarySearch(mid + 1, high);
-  else return binarySearch(low, mid);  
-}
-
-int main() {  
-  cin >> n >> target;
-  for (int i = 0; i < n; i++){
-    cin >> arr[i];
-  }
-
-  sort(arr, arr + n);
-  cout << binarySearch(0, n - 1) << "\n"; //prints the index of the element in the given, sorted array
-
+  cout << l << endl; //loop breaks when l == r, guranteed to work since r = mid only when mid is valid
   return 0;
 }
