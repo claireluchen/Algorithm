48e6507bf159c84e0aa3f6e0bda7ce28a0dd465c
Claire Chen <81056267+claireluchen@users.noreply.github.com>
2022-02-06 14:16:09 -0500

    Update PrefixSumArray.java

--- PrefixSumArray.java
+++ PrefixSumArray.java
@@ -1,3 +1,7 @@
+/*
+if n were too big, only store points where changes happen in a vector
+ex for diff array: vt.push_back({a, 1}) to increment values starting at a by 1, then ({b, -1}) to decrement values starting at b by 1
+*/
 public class Main{
   public static void main(String[] args) throws IOException{
     Scanner in = new Scanner(System.in);
