efa655d9ce0aff247d349ec8885a85d45503b7ef
Claire Chen <81056267+claireluchen@users.noreply.github.com>
2022-02-12 23:45:59 -0500

    Create BinaryIndexTree.cpp

---
+++ BinaryIndexTree.cpp
@@ -0,0 +1,22 @@
+int n;
+int a[1005], c[1005]; //对应原数组和树状数组
+
+int lowbit(int x){ //returns the rightmost 1 and the 0s to the right of it, ex: 110100 would return 100
+    return x&(-x);
+}
+
+void updata(int i,int k){ //add k to a[i], need to update all relevant c[] that contains a[i]
+    while(i <= n){
+        c[i] += k;
+        i += lowbit(i);
+    }
+}
+
+int getsum(int i){ //get sum of a[1] to a[i]
+    int res = 0;
+    while(i > 0){
+        res += c[i];
+        i -= lowbit(i);
+    }
+    return res;
+}
